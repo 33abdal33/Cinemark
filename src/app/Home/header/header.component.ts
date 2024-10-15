75a62adab66c874d5f0de12cfbdb3fc528cb5c8c
Abdal Lizano <abdallizanomuriel08@gmail.com>
2024-10-15 00:01:37 -0500

    Update Header UI

--- src/app/Home/header/header.component.ts
+++ src/app/Home/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
