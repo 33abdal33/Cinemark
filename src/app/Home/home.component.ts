59409ed89118c2a47fdbc70d572a21f8f1b23583
Abdal Lizano <abdallizanomuriel08@gmail.com>
2024-10-15 00:49:11 -0500

    Update Home UI

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "../shared/components/footer/footer.component";
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [HeaderComponent, FooterComponent],
+    imports: [HeaderComponent, FooterComponent, MatSelectModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css'
 })
