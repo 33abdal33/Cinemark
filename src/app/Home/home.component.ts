a676bfe81fd82f4e513a7af529b8e72dc9c055c5
Abdal Lizano <abdallizanomuriel08@gmail.com>
2024-10-16 23:32:19 -0500

    update home component

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from "@angular/core";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "../shared/components/footer/footer.component";
 import { MatSelectModule } from '@angular/material/select';
+import { EventCardComponent } from "../shared/components/event-card/event-card.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [HeaderComponent, FooterComponent, MatSelectModule],
+    imports: [HeaderComponent, FooterComponent, MatSelectModule, EventCardComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css'
 })
