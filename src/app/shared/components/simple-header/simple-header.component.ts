348d4f75ac93ef50a710ec891e18c924f126fe36
Abdal Lizano <abdallizanomuriel08@gmail.com>
2024-10-16 23:33:33 -0500

    Update Simple Header

--- src/app/shared/components/simple-header/simple-header.component.ts
+++ src/app/shared/components/simple-header/simple-header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../../Home/header/header.component";
 
 @Component({
   selector: 'app-simple-header',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, HeaderComponent],
   templateUrl: './simple-header.component.html',
   styleUrl: './simple-header.component.css'
 })
